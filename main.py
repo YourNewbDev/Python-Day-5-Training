798b879028aff6c01331134407af5e4a14e486bb
YourNewbDev <126557093+YourNewbDev@users.noreply.github.com>
2023-12-01 13:16:00 +0000

    Day 5 training

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
-fruits = ["Apple", "Peach", "Pear"]
+import random
 
-for fruit in fruits:
-  print(fruit)
-  print(fruit + " Pie")
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+   'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+   'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+   'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+letter_pass = int(input("How many letters would you like in your password?"))
+symbol_pass = int(input("How many symbols would you like?"))
+number_pass = int(input("How many numbers would you like?"))
+
+# Easy Level
+# password = ""
+
+# for char in range(1, letter_pass + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, symbol_pass + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, number_pass + 1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+# Hard Level
+password_list = []
+
+for char in range(1, letter_pass + 1):
+  password_list += random.choice(letters)
+
+for char in range(1, symbol_pass + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, number_pass + 1):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is {password}")
